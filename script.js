ff0aa5b3fe1fe736fe6f7c9816ccc542a07f12fc
Yash Jadon <jadonyash2@gmail.com>
2023-11-02 23:56:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+
+const inputCity= document.querySelector(".input");
+
+const but=document.querySelector('button');
+
+const weatherIcon=document.querySelector('.weather-icon');
+
+
+const apikey="ffd7a8eb250069a40ab7539affc0675e";
+
+const api="https://api.openweathermap.org/data/2.5/weather?&units=metric";
+
+async function weatherDetail(city){
+
+    const response= await fetch(api + "&q=" + city + "&appid=" + apikey);
+
+    if(response.status=="404"){
+       document.querySelector('.error').style.display="block";
+       document.querySelector('.weather').style.display="none";
+     }
+else{
+    let data= await response.json();
+
+    document.querySelector('.temp').innerHTML= Math.round(data.main.temp) + "°c";
+    document.querySelector('.city').innerHTML=data.name;
+    document.querySelector('.humidity').innerHTML=data.main.humidity + "%";
+    document.querySelector('.wind').innerHTML=data.wind.speed + "km/h";
+    
+
+    if(data.weather[0].main=="Clear"){
+       weatherIcon.src="clear.png";
+    }
+    else if(data.weather[0].main=="Clouds"){
+        weatherIcon.src="clouds.png";
+    }
+    else if(data.weather[0].main=="Drizzle"){
+        weatherIcon.src="drizzle.png";
+        
+    }
+    else if(data.weather[0].main=="Mist"){
+        weatherIcon.src="mist.png";
+    }
+    else if(data.weather[0].main=="Rain"){
+        weatherIcon.src="rain.png";
+    }
+    document.querySelector('.error').style.display="none";
+    document.querySelector('.weather').style.display="block";
+   
+}
+    
+
+}
+but.addEventListener('click',()=>{
+    const city = inputCity.value;
+   
+    weatherDetail(city);
+
+})
+
+
+
